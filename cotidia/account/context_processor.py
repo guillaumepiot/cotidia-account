93494e9d696102c208150ce40cda2b368c8445a7
Guillaume Piot <guillaume@cotidia.com>
2016-12-18 16:13:23 +0000

    - Add context processor with account settings - Update two-step auth templates

---
+++ cotidia/account/context_processor.py
@@ -0,0 +1,15 @@
+from cotidia.account.conf import settings
+
+
+def account_settings(request):
+
+    data = {
+        "ACCOUNT_ENABLE_TWO_FACTOR": settings.ACCOUNT_ENABLE_TWO_FACTOR,
+        "ACCOUNT_ADMIN_LOGIN_URL": settings.ACCOUNT_ADMIN_LOGIN_URL,
+        "ACCOUNT_PUBLIC_LOGIN_URL": settings.ACCOUNT_PUBLIC_LOGIN_URL,
+        "ACCOUNT_FORCE_ACTIVATION": settings.ACCOUNT_FORCE_ACTIVATION,
+        "ACCOUNT_ENABLE_TWO_FACTOR": settings.ACCOUNT_ENABLE_TWO_FACTOR,
+        "ACCOUNT_FORCE_ADMIN_TWO_FACTOR": settings.ACCOUNT_FORCE_ADMIN_TWO_FACTOR,
+    }
+
+    return data
