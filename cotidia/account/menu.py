4c85d048e8ef625a490fb6e84c244848ec64f9e1
Guillaume Piot <guillaume@cotidia.com>
2017-08-08 22:35:53 +0100

    Add account menu.

--- cotidia/account/menu.py
+++ cotidia/account/menu.py
@@ -1,20 +1,51 @@
-class Menu(object):
+from django.core.urlresolvers import reverse
 
-    def __init__(self):
-        self._registry = []
 
-    def register(self, title, template, order=None):
-        self._registry.append({
-            'title': title,
-            'template': template,
-            'order': order
-            })
-
-    def items(self):
-        item_list = []
-        menu_list = sorted(self._registry, key=lambda k: k['order'])
-        for item in menu_list:
-            item_list.append(item['template'])
-        return item_list
-
-menu = Menu()
+def admin_menu(context):
+    request = context["request"]
+    return [
+        {
+            "text": context["SITE_NAME"],
+            "url": reverse("dashboard"),
+        },
+        {
+            "icon": "wrench",
+            "align_right": True,
+            "nav_items": [
+                {
+                    "text": "Users",
+                    "url": reverse("account-admin:user_list"),
+                    "permissions": ["account.add_user", "account.change_user"],
+                },
+                {
+                    "text": "Roles",
+                    "url": reverse("account-admin:group_list"),
+                    "permissions": [
+                        "perms.auth.add_group",
+                        "perms.auth.change_group"
+                    ],
+                }
+            ]
+        },
+        {
+            "text": str(request.user),
+            "align_right": True,
+            "nav_items": [
+                {
+                    "text": "Profile",
+                    "url": reverse("account-admin:edit"),
+                    "permissions": [],
+                },
+                {
+                    "text": "Change password",
+                    "url": reverse("account-admin:password_change"),
+                    "permissions": [],
+                },
+                {
+                    "text": "Logout",
+                    "url": reverse("account-admin:logout"),
+                    "permissions": [],
+                }
+            ]
+        }
+    ]
