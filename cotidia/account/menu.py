9bce0185f2e132dbc88b56bc34a237214b12cc87
Guillaume Piot <guillaume@cotidia.com>
2017-12-04 12:33:21 +0000

    Add icons for sun-nav,

--- cotidia/account/menu.py
+++ cotidia/account/menu.py
@@ -11,11 +11,13 @@ def admin_menu(context):
             "nav_items": [
                 {
                     "text": "Users",
+                    "icon": "users",
                     "url": reverse("account-admin:user_list"),
                     "permissions": ["account.add_user", "account.change_user"],
                 },
                 {
                     "text": "Roles",
+                    "icon": "key",
                     "url": reverse("account-admin:group_list"),
                     "permissions": [
                         "perms.auth.add_group",
@@ -31,16 +33,19 @@ def admin_menu(context):
             "nav_items": [
                 {
                     "text": "Profile",
+                    "icon": "id-card",
                     "url": reverse("account-admin:edit"),
                     "permissions": [],
                 },
                 {
                     "text": "Change password",
+                    "icon": "unlock-alt",
                     "url": reverse("account-admin:password_change"),
                     "permissions": [],
                 },
                 {
                     "text": "Logout",
+                    "icon": "sign-out",
                     "url": reverse("account-admin:logout"),
                     "permissions": [],
                 }
