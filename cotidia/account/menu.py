07619ba672c05a9206c04490412f2b5a0f058651
Guillaume Piot <guillaume@cotidia.com>
2018-04-27 19:50:40 +0100

    Update menu.

--- cotidia/account/menu.py
+++ cotidia/account/menu.py
@@ -5,48 +5,16 @@ def admin_menu(context):
     request = context["request"]
     return [
         {
-            "icon": "wrench",
-            "text": "Settings",
-            "align_right": True,
-            "nav_items": [
-                {
-                    "text": "Users",
-                    "url": reverse("account-admin:user-list"),
-                    "permissions": ["account.add_user", "account.change_user"],
-                },
-                {
-                    "text": "Roles",
-                    "url": reverse("account-admin:group-list"),
-                    "permissions": [
-                        "perms.auth.add_group",
-                        "perms.auth.change_group"
-                    ],
-                }
-            ]
+            "text": "Users",
+            "url": reverse("account-admin:user-list"),
+            "permissions": ["account.add_user", "account.change_user"],
         },
         {
-            "text": str(request.user),
-            "icon": "user",
-            "align_right": True,
-            "nav_items": [
-                {
-                    "text": "Profile",
-                    "icon": "id-card",
-                    "url": reverse("account-admin:edit"),
-                    "permissions": [],
-                },
-                {
-                    "text": "Change password",
-                    "icon": "key",
-                    "url": reverse("account-admin:password-change"),
-                    "permissions": [],
-                },
-                {
-                    "text": "Logout",
-                    "icon": "sign-out",
-                    "url": reverse("account-admin:logout"),
-                    "permissions": [],
-                }
-            ]
+            "text": "Roles",
+            "url": reverse("account-admin:group-list"),
+            "permissions": [
+                "perms.auth.add_group",
+                "perms.auth.change_group"
+            ],
         }
     ]
