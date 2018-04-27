eae60dac39e891709f01478adbfdcbf1fb41cbfc
Guillaume Piot <guillaume@cotidia.com>
2018-04-27 20:40:32 +0100

    Update menu.

--- cotidia/account/menu.py
+++ cotidia/account/menu.py
@@ -5,16 +5,23 @@ def admin_menu(context):
     request = context["request"]
     return [
         {
-            "text": "Users",
-            "url": reverse("account-admin:user-list"),
-            "permissions": ["account.add_user", "account.change_user"],
-        },
-        {
-            "text": "Roles",
-            "url": reverse("account-admin:group-list"),
-            "permissions": [
-                "perms.auth.add_group",
-                "perms.auth.change_group"
-            ],
+            "icon": "wrench",
+            "text": "Settings",
+            "align_right": True,
+            "nav_items": [
+                {
+                    "text": "Users",
+                    "url": reverse("account-admin:user-list"),
+                    "permissions": ["account.add_user", "account.change_user"],
+                },
+                {
+                    "text": "Roles",
+                    "url": reverse("account-admin:group-list"),
+                    "permissions": [
+                        "perms.auth.add_group",
+                        "perms.auth.change_group"
+                    ],
+                }
+            ]
         }
     ]
